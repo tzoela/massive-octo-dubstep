f453383c298781c5f9ae7d01874daaad1672f189
subsidel <joelt.howard@gmail.com>
2015-06-17 23:04:43 +0100

    change port to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-var server = app.listen(8000, function () {
+var server = app.listen(8080, function () {
 
     var host = server.address().address;
     var port = server.address().port;
