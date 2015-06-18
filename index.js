96c1916a8a83485b5ae9a052520be03b40043f60
subsidel <joelt.howard@gmail.com>
2015-06-18 20:38:38 +0100

    handlebarsify it, flatten it :)

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var express = require('express');
-var app = express();
+var exphbs  = require('express-handlebars');
+var teamMembers = require('./teamMembers');
 
+var app = express();
 app.use(express.static('public'));
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/about', function (req, res, next) {
+    res.render('partials/about', {
+        teamMember: teamMembers.members
+    });
+});
+
+app.get('/', function (req, res, next) {
+    res.render('partials/index');
+});
 
 var server = app.listen(8080, function () {
 
---
+++ teamMembers.js
@@ -0,0 +1,75 @@
+module.exports = {
+    members: [
+        {
+            "name": "Crystal",
+            "website": "http://thebeatofmyowndrummer.tumblr.com/",
+            "image": "http://static.tumblr.com/6bc8321c6d8e8d163f27895e2df54d57/cgcdypu/vMYn5aij1/tumblr_static_9b09ag84wq8sk8owcs08ks88c.jpg"
+        },
+        {
+            "name": "Tori",
+            "website": "http://im-up-in-space.tumblr.com/",
+            "image": "http://31.media.tumblr.com/avatar_4547c0a22d7b_64.png"
+        },
+        {
+            "name": "Sophia",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Camilla",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Rebecca",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Tyler",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Deanne",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Aurora",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Savannah",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Alex",
+            "website": "https://twitter.com/AlexRose312",
+            "image": "https://pbs.twimg.com/profile_images/608734472285122560/JdscF22z.jpg"
+        },
+        {
+            "name": "Leila",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Joel",
+            "website": "https://twitter.com/Subsidel",
+            "image": "https://pbs.twimg.com/profile_images/601058397333618688/ZIL_Te2h.jpg"
+        },
+        {
+            "name": "Katie",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        },
+        {
+            "name": "Jodie",
+            "website": "",
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+        }
+    ]
+
+};
