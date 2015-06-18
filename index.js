e83d4d269449cfc12779cab3969a665136fa5e7a
subsidel <joelt.howard@gmail.com>
2015-06-19 00:26:48 +0100

    added showcase

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var exphbs  = require('express-handlebars');
 var teamMembers = require('./data/teamMembers');
 var gifwhes = require('./data/gifwhes');
 var gishwhes = require('./data/gishwhes');
+var showcase = require('./data/showcase');
 
 var app = express();
 app.use(express.static('public'));
@@ -13,6 +14,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
+app.get('/showcase', function(req, res, next) {
+    res.render('partials/showcase', {
+        images: showcase
+    });
+});
 
 app.get('/gifwhes', function(req, res, next) {
     res.render('partials/submissions', {
