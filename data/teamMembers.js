463106edd5fcd5d8caf4e66c57f402738291a1d4
subsidel <joelt.howard@gmail.com>
2015-06-19 23:29:13 +0100

    added more of them stuff to the abouts, with quotes and stuff

--- data/teamMembers.js
+++ data/teamMembers.js
@@ -1,6 +1,11 @@
 module.exports = {
     members: [
         {
+            "name": "Jennifer",
+            "website": "http://i-dont-wike-it.tumblr.com/",
+            "image": "https://igcdn-photos-h-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-19/10554148_1425160687779023_46641106_a.jpg"
+        },
+        {
             "name": "Crystal",
             "website": "http://thebeatofmyowndrummer.tumblr.com/",
             "image": "http://static.tumblr.com/6bc8321c6d8e8d163f27895e2df54d57/cgcdypu/vMYn5aij1/tumblr_static_9b09ag84wq8sk8owcs08ks88c.jpg"
@@ -12,8 +17,9 @@ module.exports = {
         },
         {
             "name": "Sophia",
-            "website": "",
-            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+            "website": "https://twitter.com/senrios",
+            "image": "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/1623756_966250740094384_8960420225231015297_n.jpg?oh=cde56c411952f615e6181d965d73e3c7&oe=55F557A5",
+            "quote": "GISH... GISH never changes"
         },
         {
             "name": "Camilla",
@@ -58,18 +64,21 @@ module.exports = {
         {
             "name": "Joel",
             "website": "https://twitter.com/Subsidel",
-            "image": "https://pbs.twimg.com/profile_images/601058397333618688/ZIL_Te2h.jpg"
+            "image": "https://pbs.twimg.com/profile_images/601058397333618688/ZIL_Te2h.jpg",
+            "quote": "Boy you better put a lid on it"
         },
         {
             "name": "Katie",
             "website": "",
-            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
+            "image": "https://s3.amazonaws.com/FringeBucket/default-user.png",
+            "quote": "Why does Joel want onions from me?!"
         },
         {
             "name": "Jodie",
             "website": "",
             "image": "https://s3.amazonaws.com/FringeBucket/default-user.png"
         }
-    ]
+    ],
+    helpers: []
 
 };
