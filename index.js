26443251c83099825bfe3e666b9fe7ab8947c2a5
subsidel <joelt.howard@gmail.com>
2015-06-21 22:51:00 +0100

    refactor index to use expressServer

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var cache = require('memory-cache');
-
+var expressServer = require('./lib/expressServer');
 var router = require('./lib/router');
 var setUpHeaders = require('./lib/setUpHeaders');
 
-var app = express();
-app.use(express.static('public'));
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-
-app.set('view engine', 'handlebars');
-
-setUpHeaders(app);
-router(app, cache);
-
-var server = app.listen(8080, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('listening at http://%s:%s', host, port);
-});
+expressServer.use([setUpHeaders, router]);
+expressServer.listen();
